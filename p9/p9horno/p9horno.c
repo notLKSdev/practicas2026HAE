a6262139b815939e9df60b99ca62c678283429dc
Lookos <lookoszero@gmail.com>
2026-04-20 17:06:00 +0200

    Configuracion de timer de 0.1s

--- p9/p9horno/p9horno.c
+++ p9/p9horno/p9horno.c
@@ -1,11 +1,31 @@
+const int ALPHA_TMR = 28036;
+
 void interrupt(){
 
 
 
+    if(INTCON.TMR0IF == 1){
+
+
+        INTCON.TMR0IF = 0;
+    }
+
 }
 
 void main(){
+    // Configurar timer
+    T0CON = 0x81;
 
+    // Cargar valor alfa del timer
+    TMR0L = ALPHA_TMR;
+    TMR0H = (ALPHA_TMR >> 8);
+
+    // Habilitar interrupciones timer
+    INTCON.TMR0IF = 0;
+    INTCON.TMR0IE = 1;
+
+    // Habilitar interrupciones en general
+    INTCON.GIE = 1;
 
     while(1){
 
