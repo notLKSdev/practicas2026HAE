9f040889e183de06ad9f06ee040e75ae34ccd185
notLKS <lookoszero@gmail.com>
2026-04-27 17:55:31 +0200

    Variable de estado en timer

--- p9/p9horno/p9horno.c
+++ p9/p9horno/p9horno.c
@@ -1,11 +1,13 @@
 const int ALPHA_TMR = 28036;
 const float LAMBDA_AD = 0.0048875;
 
-int e = 0;
-int c = 0;
-int t = 0;
-int tMax = 0;
-int tMin = 0;
+int e = 0; // Enable
+int c = 0; // Activar calentador
+int t = 0; // Temperatura
+int tMax = 0; // Temperatura maxima
+int tMin = 0; // Temperatura minima
+
+int Q = 0; // Variable de estado
 
 void configController(){
     ADCON0 = 0x44;
@@ -28,7 +30,20 @@ void interrupt(){
 
 
     if(INTCON.TMR0IF == 1){
+        if(Q == 0){
+            c = 0;
+            if(e == 0){
+                Q = 0;
+            }else{
+                Q = 1;
+            }
 
+        }else if(Q == 1){
+
+
+        }else{
+
+        }
 
         INTCON.TMR0IF = 0;
     }
