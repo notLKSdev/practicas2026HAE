14289f32da95902dfee1378bc6594cb50a8b38a7
Lookos <lookoszero@gmail.com>
2026-05-14 21:07:21 +0200

    Create boletin3_ej8.c

---
+++ extras/boletin3_ej8.c
@@ -0,0 +1,147 @@
+/* Este ejercicio corresponde al ejercicio 8 del boletin 3 (el que es de un sensor SRF04)
+ *
+ * ATENCION: NO LO HE PROBADO, solo lo he pensado asi y ni siquiera estoy seguro de que este bien
+ */
+
+
+#include <stdbool.h>
+
+// LCD pinout settings
+sbit LCD_RS at RD2_bit;
+sbit LCD_EN at PORTD.B3;
+sbit LCD_D7 at RD7_bit;
+sbit LCD_D6 at RD6_bit;
+sbit LCD_D5 at RD5_bit;
+sbit LCD_D4 at RD4_bit;
+
+// LCD directions
+sbit LCD_RS at TRISD2_bit;
+sbit LCD_EN at TRISD.B3;
+sbit LCD_D7 at TRISD7_bit;
+sbit LCD_D6 at TRISD6_bit;
+sbit LCD_D5 at TRISD5_bit;
+sbit LCD_D4 at TRISD4_bit;
+
+volatile unsigned int tiempoEcho = 0;
+
+float distancia = 0;
+
+char aux[15];
+
+void interrupt(){
+
+    // Se pulsa el boton
+    if(INTCON.INT0IF == 1){
+        
+        // Trigger ON
+        PORTA.B3 = 1;
+
+        // Contar 12 microsegundos en el timer
+        T0CON = 0x48;
+        TMR0H = (232 >> 8);
+        TMR0L = 232;
+
+        // Activar timer0
+        INTCON.TMR0IF = 0;
+        T0CON.TMR0ON = 1;
+
+        INTCON.INT0IF = 0;
+    }
+
+    // Terminan los 12 microsegundos del trigger
+    if(INTCON.TMR0IF == 1){
+        T0CON.TMR0ON = 0;
+
+        // Trigger OFF
+        PORTA.B3 = 0;
+
+        // Timer0 en modo medicion de echo
+        T0CON = 0x08;
+        TMR0H = 0;
+        TMR0L = 0;
+
+        // Habilitar INT2 como flanco de subida
+        INTCON.INTEDG2 = 1;
+        INTCON3.INT2IF = 0;
+        INTCON3.INT2IE = 0;
+
+        INTCON.TMR0IF = 0;
+    }
+
+    // Medimos el tiempo de echo
+    if(INTCON3.INT2IF == 1){
+
+        // Flanco de subida arrancar el timer
+        if(INTCON2.INTEDG2 == 1){
+            TMR0H = 0;
+            TMR0L = 0;
+            T0CON.TMR0ON = 1;
+
+
+            // Cambiar a flanco de bajada
+            INTCON2.INTEDG2 == 0;
+        
+        // Flanco de bajada medir el timer
+        }else{
+            T0CON.TMR0ON = 0;
+
+            // Leer el timer
+            unsigned char low = TMR0L;
+            unsigned char high = TMR0H;
+
+            unsigned int alfaEcho = ((unsigned int) high << 8) | low;
+
+            // Convertir medicion del timer a segundos
+            tiempoEcho = (unsigned long)(65536UL - alfaEcho) * 128UL;
+
+            // Calcular distancia mediante la formula
+            distancia = (1000000 * tiempoEcho)/(0,000058);
+
+            // Mostrarla por el LCD
+            Lcd_Cmd(_Lcd_Clear);
+            FloatToStr(distancia,txt);
+            Lcd_Out(1,1,txt);
+        }
+
+
+        INTCON3.INT2IF = 0;
+    }
+
+
+}
+
+void main(){
+    Lcd_init();
+    
+    // Salidas
+    TRISA.B3 = 0;
+
+    // Entradas
+    TRISB.B0 = 1;
+    TRISB.B2 = 1;
+
+    // Configuracion de boton
+    INTCON2.INTEDG0 = 1;
+    INTCON.INT0IF = 0;
+    INTCON.INT0IE = 1;
+
+    // Señal echo deshabilitada inicialmente
+    INTCON2.INTEDG2 = 1;
+    INTCON3.INT2IF  = 0;
+    INTCON3.INT2IE  = 0;
+
+    // Configuracion inicial timer0
+    T0CON = 0x48;
+    TMR0H = 0;
+    TMR0L = 232;
+    INTCON.TMR0IF = 0;
+    INTCON.TMR0IE = 1;
+
+    // Interrupciones globales y perifericas
+    INTCON.GIE  = 1;
+    INTCON.PEIE = 1;
+
+    while(1){
+
+    }
+}
