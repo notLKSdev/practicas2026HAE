dcff279ee0c555d042ba4d59152270c8eee785cd
Lookos <lookoszero@gmail.com>
2026-05-19 23:31:49 +0200

    Realizar medicion

--- extras/ex24ej3.c
+++ extras/ex24ej3.c
@@ -6,7 +6,7 @@
  * representa en el LCD.
  * Pulsar el botón antes de que se envie al LCD no debe hacer efecto.
  * 
- * Formula para calcular la humedad a partir del voltaje: v0 = 1 +HR/25
+ * Formula para calcular la humedad a partir del voltaje: v0 = 1 + HR/25
  * Frecuencia oscilacion del micro: Fosc = 16 MHz
  * Frecuencia del convertidor AD: FAD = 625 * 10^3 H
  */
@@ -28,15 +28,21 @@ sbit LCD_D5_Direction at TRISD5_bit;
 sbit LCD_D4_Direction at TRISD4_bit;
 
 char txt[15];
+unsigned int v0 = 0;
+float aux = 0.0;
 
 void interrupt(){
 
-    // Si pulsamos el boton 0 encendemos el led y activamos temporizacion de 3 segundos
+    // Si pulsamos el boton 0
     if(INTCON.INT0IF == 1){
         
         // Solo funciona si la temporizacion no esta activa
         if(INTCON.INT0IE == 1){
-            INTCON.INT0IE = 0; // Desactivamos el boton una vez pulsado hasta que ocurra la temporizacion
+            
+            // Deshabilitar boton
+            INTCON.INT0IE = 0;
+            
+            // Encender LED y activar timer de 3 se
             PORTE.B1 = 1;
             T0CON.TMR0ON = 1;
             TMR0H = (18661 >> 8);
@@ -46,21 +52,40 @@ void interrupt(){
         INTCON.INT0IF = 0;
     }
 
-    // Una vez pasen los 3 segundos apagamos el led, apagamos el timer y desbloqueamos el boton 0
+    // Una vez pasen los 3 segundos
     if(INTCON.TMR0IF == 1){
         PORTE.B1 = 0;
 
         T0CON.TMR0ON = 0;
-        INTCON.INT0IE = 1; // Volvemos a activar el boton
+        INTCON.INT0IE = 1;
         INTCON.TMR0IF = 0;
     }
 
-    // Si pulsamos el boton 1 activamos el convertidor AD
+    // Si pulsamos el boton 1
     if(INTCON3.INT1IF == 1){
 
-
+        if(INTCON3.INT1IE == 1){
+            INTCON3.INT1IE = 0;
+            ADCON0.GO = 1;
+        }
 
         INTCON3.INT1IF = 0;
+    }
+
+    // Se realiza la medicion del AD tras pulsar el boton 1
+    if(PIR1.ADIF == 1){
+
+        // Leer y calcular la humedad
+        v0 = ADRESL + (ADRESH << 8);
+        aux = 25 * ((v0 * 0.00488758) - 1);
+
+        // Escribirla en el Lcd
+        FloatToStr(aux,txt);
+        Lcd_Cmd(_Lcd_Clear);
+        Lcd_Out(1,1,txt);
+        
+        INTCON3.INT1IE = 1;
+        PIR1.ADIF = 0;
     }
 
 
